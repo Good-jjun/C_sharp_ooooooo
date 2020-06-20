52cbda498e52add111031a0f5b0a6cff085ffc7c
Good-jjun <jjun3671@naver.com>
2020-06-20 20:06:41 +0900

    C#

---
+++ 14주차/20203202최준혁_과제 1/20203202최준혁_과제 1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20203202최준혁_과제_1
+{
+    public partial class Form1 : Form
+    {
+        bool isDrag = false;
+        Point PreviousPoint;
+        Pen MyPen = new Pen(Color.Black, 2);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripSeparator1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrag = true;
+            PreviousPoint = new Point(e.X, e.Y);
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrag = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrag)
+            {
+                Point CurrentPoint = new Point(e.X, e.Y);
+                Graphics g = pictureBox1.CreateGraphics();
+                g.DrawLine(MyPen, PreviousPoint, CurrentPoint);
+                PreviousPoint = CurrentPoint;
+            }
+        }
+
+        private void 굵기10ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MyPen.Width = 10;
+        }
+
+        private void 굵기5ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MyPen.Width = 5;
+        }
+
+        private void 굵기3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MyPen.Width = 3;
+        }
+
+        private void 굵기1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MyPen.Width = 1;
+        }
+
+        private void dotToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MyPen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+        }
+
+        private void dashDotToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MyPen.DashStyle = System.Drawing.Drawing2D.DashStyle.DashDot;
+        }
+
+        private void solidToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MyPen.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+        }
+
+        private void colorCToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                MyPen.Color = colorDialog1.Color;
+            }
+        }
+
+        private void 종료XToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ 14주차/20203202최준혁_실습예제 3번/20203202최준혁_실습예제 3번/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20203202최준혁_실습예제_3번
+{
+    public partial class Form1 : Form
+    {
+        bool isDrag = false;
+        Point PreviousPoint;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrag = true;
+            PreviousPoint = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrag = false;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrag)
+            {
+                Pen MyPen;
+                Point CurrentPoint = new Point(e.X, e.Y);
+                if (comboBox1.SelectedIndex == 0)
+                    MyPen = new Pen(Color.Black);
+                else if (comboBox1.SelectedIndex == 1)
+                    MyPen = new Pen(Color.Red);
+                else if (comboBox1.SelectedIndex == 2)
+                    MyPen = new Pen(Color.Blue);
+                else
+                    MyPen = new Pen(Color.Yellow);
+
+                if (radioButton2.Checked == true)
+                    MyPen.Width = 3;
+
+                if (comboBox2.SelectedIndex == 0)
+                    MyPen.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+                else if (comboBox2.SelectedIndex == 1)
+                    MyPen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+                else
+                    MyPen.DashStyle = System.Drawing.Drawing2D.DashStyle.DashDot;
+
+                Graphics g = panel1.CreateGraphics();
+                g.DrawLine(MyPen, PreviousPoint, CurrentPoint);
+                PreviousPoint = CurrentPoint;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+        }
+    }
+}
